8186e124a78b34e2881928cd606de469ffa91ea1
AmanSalman <rapamansalman@gmail.com>
2024-09-01 13:58:40 +0300

    Update clearRes.js

--- src/client/js/clearRes.js
+++ src/client/js/clearRes.js
@@ -1,18 +1,13 @@
 export function clearResults() {
-    // Clear weather results
     document.getElementById('weather-results').innerHTML = '';
 
-    // Clear all images in the container
     const imageContainer = document.getElementById('image-container');
-    imageContainer.innerHTML = ''; // Remove all images
+    imageContainer.innerHTML = '';
 
-    // Clear trip length
     document.getElementById('trip-length').textContent = '';
 
-    // Reset the form
     document.getElementById('travel-form').reset();
 
-    // Remove all dynamically added destination fields
     const additionalDestinationFields = document.querySelectorAll('#destinations .destination:not(:first-child)');
     additionalDestinationFields.forEach(field => field.remove());
 }

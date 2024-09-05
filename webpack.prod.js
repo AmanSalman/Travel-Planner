ee62c85877f52bb7c7c33fa53df51c024136512b
AmanSalman <rapamansalman@gmail.com>
2024-09-05 19:09:28 +0300

    delete unimportant comments

--- webpack.prod.js
+++ webpack.prod.js
@@ -27,8 +27,6 @@ module.exports = merge(common, {
     optimization: {
         minimize: true,
         minimizer: [
-            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-            // `...`,
             new CssMinimizerPlugin(),
             new TerserPlugin()
         ],
